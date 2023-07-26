ff0f463d727371aee1bd70d5c298e2704edfb7d8
Pavel Davydov <Davydov.works@gmail.com>
2023-07-26 03:20:37 -0400

    add code + change pom.xml

---
+++ src/main/java/ru/netology/statistic/Main.java
@@ -0,0 +1,10 @@
+package ru.netology.statistic;
+
+public class Main {
+    public static void main(String[] args) {
+        long [] incomes = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+StatisticsService service = new StatisticsService();
+long maxResult = service.findMax(incomes);
+        System.out.println("Maximum = " + maxResult );
+    }
+}
